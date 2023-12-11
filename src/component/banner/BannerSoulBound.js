4425393fd6c2d11aa1ccd5ab8f1683bc3917e6e3
Falcon <matias.falcone@gmail.com>
2023-12-11 11:20:15 +1100

    Update BannerSoulBound.js
    Updated Ideascale link.

--- src/component/banner/BannerSoulBound.js
+++ src/component/banner/BannerSoulBound.js
@@ -10,7 +10,7 @@ const BannerAdaSouls = () => {
                 <div className="banner-content">
                     <h1 className="title">Soulbound</h1>
                     <p className="description">Unlock the Power of Non-Transferable Tokens in Cardano.<br></br><br></br>Empowering DAOs on Cardano: Soulbound Tokens for True Community Reputation!</p>
-                    <a href={"https://cardano.ideascale.com/c/idea/103296"} className="axil-btn btn-fill-green btn-large">Project Catalyst Proposal</a>
+                    <a href={"https://cardano.ideascale.com/c/idea/110101"} className="axil-btn btn-fill-green btn-large">Project Catalyst Proposal</a>
                 </div>
             </div>
         </div>
