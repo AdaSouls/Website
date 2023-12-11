35f1e4b9d99c6837d0115e82455c599f6d8800be
Falcon <matias.falcone@gmail.com>
2023-12-11 11:19:47 +1100

    Update BannerPoap.js
    Updated Ideascale link.

--- src/component/banner/BannerPoap.js
+++ src/component/banner/BannerPoap.js
@@ -10,7 +10,7 @@ const BannerAdaSouls = () => {
                 <div className="banner-content">
                     <h1 className="title">POAP</h1>
                     <p className="description">POAP on Cardano: Remember, Relive, and Reward Your Experiences!<br></br><br></br>Unlock Your Cardano Journey with SPOAP: Evolving NFTs for Every Milestone!</p>
-                        <a href={"https://cardano.ideascale.com/c/idea/100932/"} className="axil-btn btn-fill-lightblue btn-large">Project Catalyst Proposal</a>
+                        <a href={"https://cardano.ideascale.com/c/idea/111292"} className="axil-btn btn-fill-lightblue btn-large">Project Catalyst Proposal</a>
                 </div>
             </div>
         </div>
