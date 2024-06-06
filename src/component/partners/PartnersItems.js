120e7ab4ef42e8a2798ad9aa77abb404b6d2890f
Mmoouu <mauro.hfalcone@gmail.com>
2024-06-06 14:27:51 -0300

    Add Partner AdaSolar

--- src/component/partners/PartnersItems.js
+++ src/component/partners/PartnersItems.js
@@ -24,6 +24,10 @@ const Data = [
     {
         image: "/images/brand/falcon.png",
         link: "https://FalconStakepool.com"
+    },
+    {
+        image: "/images/brand/adasolar.png",
+        link: "https://linkedin.com/in/adasolar"
     }
 ]
 
@@ -32,7 +36,7 @@ const PartnersItems = () => {
     return (
         <>
             {Data.map((data, index) => (
-                <div className="col-lg-4 col-6" key={index}>
+                <div className="col-lg-3 col-6" key={index}>
                     <div className="brand-grid">
                         <a target={'_blank'} href={process.env.PUBLIC_URL + data.link} rel="noreferrer"><img src={process.env.PUBLIC_URL + data.image} alt="Brand" /></a>
                     </div>
